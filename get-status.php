6b6b3d1861e2c0814f187117c4e5091c85f82d59
Tim Rodger <tim.rodger@gmail.com>
2015-04-21 22:24:26 +0100

    Adds accessing authn protected repos

--- get-status.php
+++ get-status.php
@@ -2,9 +2,34 @@
 
 require(__DIR__.'/src/vendor/autoload.php');
 
+function usage($msg)
+{
+    print "get-status.php report on the status of a repo's build on travis\n";
+    print "usage: get-status.php repo-name...\n";
+    print "\n$msg\n";
+    exit;
+}
+
+if (count($argv) < 2){
+    usage('Error: needs repo arg');
+}
+
+$config = json_decode(file_get_contents(__DIR__ . '/config.json'), true);
+
+var_dump($config);
+
 use Tools\TravisClient;
 
-$client = new TravisClient(false);
+$client = new TravisClient($config['github_token'], $private = false);
 
-$status = $client->getBuildStatus('ArtCoeur/codewords-board');
-var_dump($status);
+$app = array_shift($argv);
+
+// get the status of all repos on the command line
+while ($repo = array_shift($argv)) {
+    $status = $client->getBuildStatus($repo);
+    if (isset($status['error'])){
+        printf("%s error %s\n", $repo, $status['error']);
+    } else {
+        printf("%s %s at %s\n", $repo, $status['state'], $status['finished_at']);
+    }
+}
--- src/Tools/TravisClient.php
+++ src/Tools/TravisClient.php
@@ -2,6 +2,7 @@
 
 use Guzzle\Http\Client;
 use Exception;
+use Guzzle\Http\Message\Request;
 
 /**
  * Allows interaction with travis ci
@@ -9,46 +10,108 @@ use Exception;
 class TravisClient
 {
     /**
+     * Whether or not to use the private repo endpoint
+     *
      * @var bool
      */
     private $private;
 
-    private  $ua = 'Tools-Client/1.0.0';
+    private $ua = 'Tools-Client/1.0.0';
 
     private $accept = 'application/vnd.travis-ci.2+json';
 
-    private $public_endpoint = 'https://api.travis-ci.org';
+    private $public_host = 'api.travis-ci.org';
 
-    private $private_endpoint = '';
+    private $private_host = 'api.travis-ci.com';
 
-    public function __construct($private = false)
+    private $github_token;
+
+    private $travis_token = null;
+
+    /**
+     * @param bool $private
+     */
+    public function __construct($token, $private = false)
     {
+        $this->github_token = $token;
         $this->private = $private;
     }
 
+    /**
+     * Get the status of the last travis build for this repo
+     *
+     * @param $repo
+     * @return string
+     */
     public function getBuildStatus($repo)
     {
-        $pattern = '%s/repos/%s/builds';
+        $pattern = 'https://%s/repos/%s/builds';
 
         if ($this->private){
-            $uri = sprintf($pattern, $this->private_endpoint, $repo);
+            $uri = sprintf($pattern, $this->private_host, $repo);
         } else {
-            $uri = sprintf($pattern, $this->public_endpoint, $repo);
+            $uri = sprintf($pattern, $this->public_host, $repo);
         }
 
-        $client = new Client();
-        $request = $client->createRequest(
-            'GET',
-            $uri,
-            ['Accept' => $this->accept, 'User-Agent' => $this->ua]);
+        $response = $this->makeRequest("GET", $uri);
+
+        if (!isset($response['error'])) {
+            $last_build = array_shift($response['builds']);
+            return ['state' => $last_build['state'], 'finished_at' => $last_build['finished_at']];
+        } else {
+            return ['state' => 'unknown', 'finished_at' => 'unknown', 'error' => $response['error']];
+        }
+    }
+
+    private function getTravisToken()
+    {
+        if (!$this->travis_token){
+            $response = $this->makeRequest(
+                'POST',
+                sprintf("https://%s/auth/github", $this->public_host),
+                ['github_token' => $this->github_token],
+                false
+            );
+
+            if (!$response) {
+                throw new Exception("Can't get travis token");
+            }
+
+            return $response['access_token'];
+        }
+        return $this->travis_token;
+    }
+
+    /**
+     * @param $method
+     * @param $uri
+     * @param $body
+     * @param bool $authn
+     *
+     * @return mixed|string
+     */
+    private function makeRequest($method, $uri, $body = null, $authn = true)
+    {
+        $headers = ['Accept' => $this->accept, 'User-Agent' => $this->ua];
+
+        if ($this->private && $authn){
+            // add an authn header
+            $token = $this->getTravisToken();
+            $headers['Authorization'] = "token $token";
+        }
+
+        if (is_array($body)){
+            $body = json_encode($body);
+            $headers['Content-Type'] = 'application/json';
+        }
 
         try {
+            $client = new Client();
+            $request = $client->createRequest($method, $uri, $headers, $body);
             $response = $request->send();
-            $data = json_decode($response->getBody(true), true);
-            $last_build = array_shift($data['builds']);
-            return $last_build['state'];
+            return json_decode($response->getBody(true), true);
         } catch (Exception $ex){
-            return 'unknown';
+            return ['error' => $ex->getMessage()];
         }
     }
 }
